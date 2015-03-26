ce0aa7d5a5d1ce656c1917f1465b5cb7c9e8a3ed
yiliangz <yiliangz@qq.com>
2015-03-26 17:29:25 +0800

    加入ehcache

--- champions/src/main/java/com/summer/champions/domain/Player.java
+++ champions/src/main/java/com/summer/champions/domain/Player.java
@@ -2,27 +2,20 @@ package com.summer.champions.domain;
 
 
 import com.summer.common.domain.IdEntity;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 
 /**
  * Created by Allen on 2015/3/14.
  */
 @Entity
 @Table(name = "PLAYER")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class Player extends IdEntity{
-
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    protected Long id;
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
 
     public String name;
 
